b64b41cae1537f6417c6fc2b16adac8c28db4c0d
Edwin Kato <katedwin88@gmail.com>
2017-04-18 12:07:56 +0300

    Implemented generate prime number function

--- prime_number_generator.py
+++ prime_number_generator.py
@@ -1,4 +1,15 @@
 def generate_prime_numbers(n):
     """Function to generate prime numbers"""
 
+    j = 2
+    prime_numbers = []
+    while j <= n:
+        k = 2
+        while not(k == j) and not(j%k == 0):
+            k = k + 1
+        if k == j:
+            prime_numbers.append(j)
+        j = j + 1
+    print prime_numbers
+
 generate_prime_numbers(10)
